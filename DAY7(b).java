66c9a89fc877031debb37439b6aadd308da98e5a
sivakrishnaatkuri-commits <sivakrishnaatkuri@gmail.com>
2026-07-24 16:08:55 +0530

    Add files via upload

---
+++ DAY7(b).java
@@ -0,0 +1,18 @@
+// Private Access Modifier
+// public class Main{
+//     private int x=10;
+//     public static void main(String[] args){
+//         Main obj = new Main();
+//         System.out.println(obj.x);
+//     }
+// }
+
+public class Main{
+    public static void main(String[] args){
+        class108 obj = new class108();
+        System.out.println(obj.x);
+    }
+}
+public class class108{
+    private int x = 12;
+}
